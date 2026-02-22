904cb160a17bdecdcb0cb0b8ccb957fe30591e55
NYI NYI OO <30399841+NyiOo@users.noreply.github.com>
2026-02-22 20:54:20 +0630

    Refactor account tests for clarity and consistency

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -126,48 +126,50 @@ class TestAccountService(TestCase):
     # ADD YOUR TEST CASES HERE ...
 
     def test_get_account(self):
-        """It should Get a single Account"""
-        # get the id of a product
-        test_account = self._create_accounts(1)[0]
-        response = self.client.get(f"{BASE_URL}/{test_account.id}")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        data = response.get_json()
-        self.assertEqual(data["name"], test_account.name)
+        """It should Read a single Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.get(
+            f"{BASE_URL}/{account.id}", content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], account.name)
+
+
+    def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
     
     def test_delete_account(self):
-        """It should Delete a Account"""
-        accounts = self._create_accounts(5)
-        account_count = self.get_account_count()
-        test_account = accounts[0]
-        response = self.client.delete(f"{BASE_URL}/{test_account.id}")
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertEqual(len(response.data), 0)
-        # make sure they are deleted
-        response = self.client.get(f"{BASE_URL}/{test_account.id}")
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-        new_count = self.get_account_count()
-        self.assertEqual(new_count, account_count - 1)
+        """It should Delete an Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.delete(f"{BASE_URL}/{account.id}")
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
     
     def test_update_account(self):
-        """It should Update an existing Account"""
-        # create a account to update
-        test_account = AccountFactory()
-        response = self.client.post(BASE_URL, json=test_account.serialize())
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
+       """It should Update an existing Account"""
+       test_account = AccountFactory()
+       resp = self.client.post(BASE_URL, json=test_account.serialize())
+       self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
         # update the account
-        new_account = response.get_json()
-        new_account["description"] = "unknown"
-        response = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        updated_account = response.get_json()
-        self.assertEqual(updated_account["description"], "unknown")
+       new_account = resp.get_json()
+       new_account["name"] = "Something Known"
+       resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+       self.assertEqual(resp.status_code, status.HTTP_200_OK)
+       updated_account = resp.get_json()
+       self.assertEqual(updated_account["name"], "Something Known")
 
     def test_get_account_list(self):
         """It should Get a list of Accounts"""
         self._create_accounts(5)
-        response = self.client.get(BASE_URL)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        data = response.get_json()
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
         self.assertEqual(len(data), 5)
+
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
