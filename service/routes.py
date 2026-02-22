7a062a8b4e8d3a96b09e54bceb8ff7a0c1a73e21
NYI NYI OO <30399841+NyiOo@users.noreply.github.com>
2026-02-22 20:54:41 +0630

    Improve account listing and retrieval logic
    Refactor account listing and retrieval endpoints for clarity and efficiency.

--- service/routes.py
+++ service/routes.py
@@ -63,32 +63,15 @@ def create_accounts():
 
 @app.route("/accounts", methods=["GET"])
 def list_products():
-    """Returns a list of Accounts"""
-    app.logger.info("Request to list Accounts...")
-
-    accounts = []
-    name = request.args.get("name")
-    email = request.args.get("email")
-    phone_number = request.args.get("phone_number")
-
-    if name:
-        app.logger.info("Find by name: %s", name)
-        products = Account.find_by_name(name)
-    elif email:
-        app.logger.info("Find by email: %s", email)
-        products = Account.find_by_email(email)
-    elif phone_number:
-        app.logger.info("Find by phone number: %s", phone_number)
-        products = Account.find_by_phone_number(phone_number)
-    
-    else:
-        app.logger.info("Find all")
-        products = Account.all()
-
-    results = [account.serialize() for account in accounts]
-    app.logger.info("[%s] Accounts returned", len(results))
-    return results, status.HTTP_200_OK
-
+   """
+    List all Accounts
+    This endpoint will list all Accounts
+    """
+   app.logger.info("Request to list Accounts")
+   accounts = Account.all()
+   account_list = [account.serialize() for account in accounts]
+   app.logger.info("Returning [%s] accounts", len(account_list))
+   return jsonify(account_list), status.HTTP_200_OK
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
@@ -96,17 +79,13 @@ def list_products():
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
-    Retrieve a single Account
-
-    This endpoint will return an Account based on it's id
+    Reads an Account
+    This endpoint will read an Account based the account_id that is requested
     """
-    app.logger.info("Request to Retrieve an account with id [%s]", account_id)
-
+    app.logger.info("Request to read an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id '{account_id}' was not found.")
-
-    app.logger.info("Returning account: %s", account.name)
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
 
 
@@ -118,18 +97,13 @@ def get_accounts(account_id):
 def update_account(account_id):
     """
     Update an Account
-
-    This endpoint will update an Account based the body that is posted
+    This endpoint will update an Account based on the posted data
     """
-    app.logger.info("Request to Update an account with id [%s]", account_id)
-    check_content_type("application/json")
-
+    app.logger.info("Request to update an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id '{account_id}' was not found.")
-
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     account.deserialize(request.get_json())
-    account.id = account_id
     account.update()
     return account.serialize(), status.HTTP_200_OK
 
@@ -142,15 +116,12 @@ def update_account(account_id):
 def delete_account(account_id):
     """
     Delete an Account
-
-    This endpoint will delete an Account based the id specified in the path
+    This endpoint will delete an Account based on the account_id that is requested
     """
-    app.logger.info("Request to Delete an account with id [%s]", account_id)
-
+    app.logger.info("Request to delete an Account with id: %s", account_id)
     account = Account.find(account_id)
     if account:
         account.delete()
-
     return "", status.HTTP_204_NO_CONTENT
 
 
