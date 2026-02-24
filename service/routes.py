d05dd752589513c11ca6e127a2fd099de84abf42
NYI NYI OO <30399841+NyiOo@users.noreply.github.com>
2026-02-24 22:38:07 +0630

    Remove unnecessary blank lines in routes.py

--- service/routes.py
+++ service/routes.py
@@ -60,22 +60,20 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-
 @app.route("/accounts", methods=["GET"])
 def list_products():
    """
     List all Accounts
     This endpoint will list all Accounts
     """
-   app.logger.info("Request to list Accounts")
-   accounts = Account.all()
-   account_list = [account.serialize() for account in accounts]
-   app.logger.info("Returning [%s] accounts", len(account_list))
-   return jsonify(account_list), status.HTTP_200_OK
+    app.logger.info("Request to list Accounts")
+    accounts = Account.all()
+    account_list = [account.serialize() for account in accounts]
+    app.logger.info("Returning [%s] accounts", len(account_list))
+    return jsonify(account_list), status.HTTP_200_OK
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
@@ -92,7 +90,6 @@ def get_accounts(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_account(account_id):
     """
@@ -111,7 +108,6 @@ def update_account(account_id):
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_account(account_id):
     """
@@ -128,8 +124,6 @@ def delete_account(account_id):
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
-
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
